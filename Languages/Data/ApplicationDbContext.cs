eaf57e9272e1e0bc46cc57ae4590f458d189af9c
mreshboboyev <mreshboboyev@gmail.com>
2023-12-09 22:41:34 +0500

    Add project files.

---
+++ Languages/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Languages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Languages.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Language> Languages { get; set; }
+    }
+}
